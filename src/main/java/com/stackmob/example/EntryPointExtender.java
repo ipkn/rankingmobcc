9f4c29884816f938d6b79732fba9ef9bb7281d20
Jaeseung Ha <ipknhama@gmail.com>
2013-03-31 07:07:57 +0900

    add wide ranking to service

--- src/main/java/com/stackmob/example/EntryPointExtender.java
+++ src/main/java/com/stackmob/example/EntryPointExtender.java
@@ -29,6 +29,7 @@ public class EntryPointExtender extends JarEntryObject {
     List<CustomCodeMethod> list = new ArrayList<CustomCodeMethod>();
     list.add(new HelloWorld());
     list.add(new Highscore());
+    list.add(new WideRanking());
     //list.add(new TwilioSMS());
     //list.add(new SendGrid());
     //list.add(new Stripe());
